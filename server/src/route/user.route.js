33972ccff93165566ada466d379bf4ee102220ed
Emmanuel Achana <achanamosey@gmail.com>
2021-06-09 11:01:32 +0000

    Added a parameter to switch between auth types.

--- server/src/route/user.route.js
+++ server/src/route/user.route.js
@@ -4,8 +4,9 @@ const UserController = require('../controller/user.controller.js');
 const {authenticate} = require('../middleware.js');
 const awaitHandlerFactory = require('../awaitHandler.js');
 const userController = require('../controller/user.controller.js');
+const {AuthType} = require('../utils.min.js');
 
-router.get('/user', authenticate(), awaitHandlerFactory(UserController.getUsers));
+router.get('/user', authenticate(AuthType.cookie), awaitHandlerFactory(UserController.getUsers));
 router.post('/user/create', awaitHandlerFactory(UserController.createUser));
 router.post('/user/login', awaitHandlerFactory(UserController.userLogin));
 router.get('/user/state', awaitHandlerFactory(userController.isUserAlive));
