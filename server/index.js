72df4deca5137a6f8065372157af5c6ae9c18ce0
Emmanuel Achana <achanamosey@gmail.com>
2021-06-09 11:03:10 +0000

    Added swagger UI.

--- server/index.js
+++ server/index.js
@@ -7,25 +7,29 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
 // Have Node serve the files for our built React app
 const root = require("path").join(__dirname, "./../client/build");
 
 app.use(express.json());
 app.use(express.static(root));
-app.use(`/api`, userRouter);
 
-//app.use(jwt({ secret: jwtSecret, algorithms: ['HS256'] }));
+app.use(`/api`, userRouter);
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
+
+app.use( '/api-docs', swaggerUi.serve,  swaggerUi.setup(swaggerDocument) );
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => { req; res.sendFile("index.html", { root }); });
 
 // Error middleware
 app.use(errorMiddleware);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
